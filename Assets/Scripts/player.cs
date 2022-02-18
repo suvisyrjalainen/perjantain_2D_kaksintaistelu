0cf44e9136ce6508c8a3fdb9b2fab291c1e2acd5
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2022-02-18 18:18:47 +0200

    animaatioita aloitettu

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	private float horizontalSpeed = 0f;
+	
+	public float speed = 5f;
+	public float JumpForce = 7.5f;
+	
+	public CircleCollider2D Feet;
+	
+	//Rigidbody for movement
+    public Rigidbody2D MyRigidbody2D;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Ottaa talteen paljonko vaakanäppäimiä on painettu
+        horizontalSpeed = Input.GetAxis("Horizontal");
+		
+		if(Input.GetButtonDown("Jump")  && Feet.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+			MyRigidbody2D.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
+		}
+		
+		//Tämä komento liikuttaa näppäinpainallusten verran
+		MyRigidbody2D.velocity = new Vector2(horizontalSpeed * speed, MyRigidbody2D.velocity.y);
+    }
+}
